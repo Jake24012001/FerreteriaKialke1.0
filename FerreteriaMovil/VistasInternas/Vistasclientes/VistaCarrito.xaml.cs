fc22656a56c71e193001559d3a4335d537623887
Jake24012001 <oberinho2401@outlook.com>
2025-02-08 13:11:41 -0500

    .

--- FerreteriaMovil/VistasInternas/Vistasclientes/VistaCarrito.xaml.cs
+++ FerreteriaMovil/VistasInternas/Vistasclientes/VistaCarrito.xaml.cs
@@ -16,16 +16,16 @@ public partial class VistaCarrito : ContentPage
 
     private async void UsarioVista_Clicked(object sender, EventArgs e)
     {
-       // await Navigation.PushAsync(new VistaCarrito());
+        await Navigation.PushAsync(new VistaUsuario());
     }
 
     private async void CategoriaVista_Clicked(object sender, EventArgs e)
     {
-       // await Navigation.PushAsync(new ViewCategoria());
+        await Navigation.PushAsync(new VistaCategoria());
     }
 
     private async void FacturaVista_Clicked(object sender, EventArgs e)
     {
-        //await Navigation.PushAsync(new VistaCarrito());
+        await Navigation.PushAsync(new VistaFactura());
     }
 }
