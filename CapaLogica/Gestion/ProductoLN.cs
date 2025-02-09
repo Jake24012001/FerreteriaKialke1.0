07beecb05aea8a9c13e92c33aace329f794c356a
jch-berrezueta <jcberrezueta8@gmail.com>
2025-02-08 22:51:22 -0500

    ProductoCD y ProductoLN hechos

--- CapaLogica/Gestion/ProductoLN.cs
+++ CapaLogica/Gestion/ProductoLN.cs
@@ -44,6 +44,58 @@ namespace CapaLogica.Gestion
             return lista;
         }
 
-       
+        public static List<Producto> filtrarProductosLN(string clave)
+        {
+            List<Producto> lista = null;
+            try
+            {
+                var sql = from x in ProductoCD.filtrarProductosCD(clave)
+                          select new Producto(x.Id_Producto, x.Id_CategoriaProducto, x.Nombre, x.Precio, x.Stock, x.Estado, x.Icono, x.Descripcion);
+                lista = sql.ToList();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error en filtrar Productos LN" + error.Message);
+            }
+            return lista;
+        }
+
+        public static void insertarProductoLN(Producto producto)
+        {
+            try
+            {
+                ProductoCD.insertarProductosCD(producto);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error en insertar Productos LN" + error.Message);
+            }
+        }
+
+        public static void modificarProductoLN(Producto producto)
+        {
+            try
+            {
+                ProductoCD.modificarProductosCD(producto);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error en modificar Productos LN" + error.Message);
+            }
+        }
+
+        public static void eliminarProductoLN(int idProducto)
+        {
+            try
+            {
+                ProductoCD.eliminarProductosCD(idProducto);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error en eliminar Productos LN" + error.Message);
+            }
+        }
+
+
     }
 }
