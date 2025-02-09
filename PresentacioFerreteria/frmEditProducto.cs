a47aedde8fe7f8e6b243fef7e73e70a515223691
Jake24012001 <oberinho2401@outlook.com>
2025-02-08 22:25:42 -0500

    vertodfo

--- PresentacioFerreteria/frmEditProducto.cs
+++ PresentacioFerreteria/frmEditProducto.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace FerreteriaAdmin
 {
     public partial class frmEditProducto : Form
@@ -15,9 +16,15 @@ namespace FerreteriaAdmin
         public frmEditProducto()
         {
             InitializeComponent();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
