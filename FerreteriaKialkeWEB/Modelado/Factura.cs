4a10a0012e0eecb267f8251f3df31b6fd99c8177
Kimberly Patricia Valladolid Chamba <Oberinho2401@outlook.com>
2025-02-09 15:55:47 -0500

    Kim

---
+++ FerreteriaKialkeWEB/Modelado/Factura.cs
@@ -0,0 +1,16 @@
+﻿namespace FerreteriaKialkeWEB.Modelado
+{
+    public class Factura
+    {
+        public int Id_Factura { get; set; }
+        public DateTime FechaEmision { get; set; }
+        public string MetodoPago { get; set; }
+        public decimal Total { get; set; }
+        public decimal Iva { get; set; }
+
+        public int Id_Cliente { get; set; }
+        public Cliente Cliente { get; set; }
+
+
+    }
+}
