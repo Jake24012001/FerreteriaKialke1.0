e78b692c19f374c439aa600f7bb86ab7faf662f4
Kimberly Patricia Valladolid Chamba <Oberinho2401@outlook.com>
2025-02-10 19:04:44 -0500

    Kim

--- FerreteriaKialkeWEB/Controladores/RolCTR.cs
+++ FerreteriaKialkeWEB/Controladores/RolCTR.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using FerreteriaKialkeWEB.Datos;
 using FerreteriaKialkeWEB.Modelado;
+using Microsoft.EntityFrameworkCore;
 
 namespace FerreteriaKialkeWEB.Controladores
 {
@@ -31,5 +32,72 @@ namespace FerreteriaKialkeWEB.Controladores
                 return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message, respuesta = listRol });
             }
         }
+
+        [HttpGet]
+        [Route("Obtener")]
+        public IActionResult Obtener(int idRol)
+        {
+            Rol oRol = context.Rols.Find(idRol);
+            if (oRol == null)
+            {
+                return BadRequest("Rol no encontrado");
+            }
+            try
+            {
+                oRol = context.Rols.Where(p => p.IdRol == idRol).FirstOrDefault();
+                
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "rol", respuesta = oRol });
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message, respuesta = oRol });
+            }
+        }
+
+        [HttpPost]
+        [Route("Guardar")]
+        public IActionResult Guardar([FromBody] Rol objeto)
+        {
+            try
+            {
+                context.Rols.Add(objeto);
+                context.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "rol" });
+
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message});
+            }
+        }
+
+
+        [HttpPost]
+        [Route("Editar")]
+        public IActionResult Editar([FromBody] Rol objeto)
+        {
+            Rol oRol = context.Rols.Find(objeto.IdRol);
+            if (oRol == null)
+            {
+                return BadRequest("Rol no encontrado");
+            }
+            try
+            {
+                oRol.Nombre = objeto.Nombre is null ? oRol.Nombre : objeto.Nombre;
+                oRol.Cuenta = objeto.Cuenta is null? oRol.Cuenta : objeto.Cuenta;
+
+
+                context.Rols.Add(objeto);
+                context.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "rol" });
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message });
+            }
+        }
+
     }
 }
